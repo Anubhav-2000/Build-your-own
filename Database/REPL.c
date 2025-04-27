235c6cc65a894f386227cf76b9073eda494d859c
Anubhav Ahlawat <ahlawat.anubhav@gmail.com>
2025-04-27 22:45:17 +0530

    Completed REPL for SQLite clone

--- Database/REPL.c
+++ Database/REPL.c
@@ -17,7 +17,26 @@ InputBuffer* new_input_buffer() {
   input_buffer->input_length = 0;
   return input_buffer;
 }
+void read_input(InputBuffer* input_buffer) {
+  ssize_t bytes_read =
+      getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
 
+  if (bytes_read <= 0) {
+    printf("Error reading input\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Ignore trailing newline
+  input_buffer->input_length = bytes_read - 1;
+  input_buffer->buffer[bytes_read - 1] = 0;
+}
+void print_prompt() { printf("db > "); }
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+
+void close_input_buffer(InputBuffer* inputBuffer){
+  free(inputBuffer->buffer);
+  free(inputBuffer);
+}
 int main(int argc, char* argv[]){
     InputBuffer* input_buffer = new_input_buffer();
   while (true) {
@@ -34,19 +53,6 @@ int main(int argc, char* argv[]){
 }
 
 
-void print_prompt() { printf("db > "); }
-ssize_t getline(char **lineptr, size_t *n, FILE *stream);
-void read_input(InputBuffer* input_buffer) {
-  ssize_t bytes_read =
-      getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
 
-  if (bytes_read <= 0) {
-    printf("Error reading input\n");
-    exit(EXIT_FAILURE);
-  }
 
-  // Ignore trailing newline
-  input_buffer->input_length = bytes_read - 1;
-  input_buffer->buffer[bytes_read - 1] = 0;
-}
 
