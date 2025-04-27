8290abf452df11c837a9d6820b911bc7c4c979e5
Anubhav Ahlawat <ahlawat.anubhav@gmail.com>
2025-04-27 14:33:12 +0530

    Add Database folder normally

---
+++ Database/REPL.c
@@ -0,0 +1,52 @@
+#include <stdlib.h> // for malloc, free, size_t
+#include <stdio.h>  // for getline, printf, FILE, etc
+#include <string.h> // for strcmp
+#include <stdbool.h>
+
+
+typedef struct {
+  char* buffer;
+  size_t buffer_length;
+  ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer() {
+  InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+  input_buffer->buffer = NULL;
+  input_buffer->buffer_length = 0;
+  input_buffer->input_length = 0;
+  return input_buffer;
+}
+
+int main(int argc, char* argv[]){
+    InputBuffer* input_buffer = new_input_buffer();
+  while (true) {
+    print_prompt();
+    read_input(input_buffer);
+
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+      close_input_buffer(input_buffer);
+      exit(EXIT_SUCCESS);
+    } else {
+      printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+    }
+  }
+}
+
+
+void print_prompt() { printf("db > "); }
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+void read_input(InputBuffer* input_buffer) {
+  ssize_t bytes_read =
+      getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+  if (bytes_read <= 0) {
+    printf("Error reading input\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Ignore trailing newline
+  input_buffer->input_length = bytes_read - 1;
+  input_buffer->buffer[bytes_read - 1] = 0;
+}
+
